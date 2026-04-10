4bd2b145df6a74b1fb61372f6305fc72e5600fec
therealaswrek <65848860+TheRealAswrek@users.noreply.github.com>
2026-04-10 21:09:29 +0200

    created map reading prototype

--- src/main.js
+++ src/main.js
@@ -2,11 +2,56 @@ var game = document.getElementById("game");
 var ctx = game.getContext("2d");
 
 //cfg
-const GAME_WIDTH = 400
-const GAME_HEIGHT = 400
-const TILE_SIZE_X = 20
-const TILE_SIZE_Y = 20
+const GAME_WIDTH = 400;
+const GAME_HEIGHT = 400;
+const TILE_SIZE = 20;
+const TILE_COLORS = {
+    "#" : "Black",    //# - ściana
+    "." : "White",    //. - podłoga
+    "S" : "Blue",    //S - start
+    "E" : "Red",    //E - wyjście
+    "H" : "Green"    //H - leczenie
+};
 //
+
+const MAP = [//TODO: Mapy jako osobny plik (.json? osobny plik .js?)
+    "####################",
+    "#S.....#..........E#",
+    "#.###..#..#######..#",
+    "#...#..#.....#.....#",
+    "###.#..#####.#.###.#",
+    "#...#......#.#...#.#",
+    "#.#######..#.###.#.#",
+    "#.....H....#.....#.#",
+    "#..######..#####.#.#",
+    "#......K.........#.#",
+    "####################"
+];
+
+function placeTile(x,y,style){
+    ctx.fillStyle = style
+
+    ctx.fillRect(x,y,TILE_SIZE,TILE_SIZE);
+}
+
+function buildMap(level){
+    //Wczytywanie mapy
+    //Mapa nie musi być kwadratem :)
+
+    let row = 0;
+    level.forEach(element => {
+        let column = 0;
+        while(element[column] != undefined){
+            placeTile(row * TILE_SIZE, column * TILE_SIZE, TILE_COLORS[element[column]]);
+            column++;
+        }
+
+        row++;
+    });
+}
+
+
+buildMap(MAP);
 
 ctx.lineWidth = 5;
 ctx.strokeRect(0,0,GAME_WIDTH,GAME_HEIGHT);

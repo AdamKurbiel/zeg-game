84c9bebcebd23a07600aaa87f61daf1002b079d2
therealaswrek <65848860+TheRealAswrek@users.noreply.github.com>
2026-04-10 20:39:26 +0200

    Basic canvas init

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("Init");
+var game = document.getElementById("game");
+var ctx = game.getContext("2d");
+
+//cfg
+const GAME_WIDTH = 400
+const GAME_HEIGHT = 400
+const TILE_SIZE_X = 20
+const TILE_SIZE_Y = 20
+//
+
+ctx.lineWidth = 5;
+ctx.strokeRect(0,0,GAME_WIDTH,GAME_HEIGHT);

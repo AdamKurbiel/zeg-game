3f94e197ace1fc1e405c3ac03a058c80dce38489
Adam Kurbiel <65848860+AdamKurbiel@users.noreply.github.com>
2026-05-15 14:48:02 +0200

    Added comments to main.js

--- src/core/main.js
+++ src/core/main.js
@@ -8,16 +8,21 @@ import { checkHudHover, checkHudClick} from "../systems/hud.js";
 import { EntityHandler } from "../systems/entityHandler.js";
 import { AudioSystem } from "../systems/audio.js";
 
-const SMOOTHING_ENABLED = false; //filtrowanie
+const SMOOTHING_ENABLED = false; //filtrowanie obrazu wyłączone żeby pixelart nie był niewyraźny.
 
-var gameCanvas = document.getElementById("game");
-var statsCanvas = document.getElementById("gameStats");
+var gameCanvas = document.getElementById("game"); //główny canvas gry
+var statsCanvas = document.getElementById("gameStats"); //canvas pokazujący ekwipunek, statystyki oraz opcje.
 
+//kontekst
 var ctx = gameCanvas.getContext("2d");
 var statsCtx = statsCanvas.getContext("2d");
+
+//ustawienia z SMOOTHING_ENABLED
 ctx.imageSmoothingEnabled = SMOOTHING_ENABLED;
 statsCtx.imageSmoothingEnabled = SMOOTHING_ENABLED;
 
+
+//tworzenie obiektów mapy, gracza, kamery, inputu, systemu bytów, systemu audio.
 const MAP = new Map();
 const PLAYER = new Player();
 const CAMERA = new Camera();
@@ -25,26 +30,30 @@ const KEYS = createKeyboard();
 const ENTITYHANDLER = new EntityHandler();
 const AUDIOSYSTEM = new AudioSystem();
 
+
+//Import czcionki tiny5
 const FONT_TINY = new FontFace('tiny5','url(assets/ui/tiny5.ttf)');
 FONT_TINY.load().then((loadedfont) =>{
     window.document.fonts.add(loadedfont);
 });
-
+//Import czcionki jersey
 const FONT_JERSEY = new FontFace('jersey','url(assets/ui/jersey.ttf)');
 FONT_JERSEY.load().then((loadedfont) =>{
     window.document.fonts.add(loadedfont);
 });
 
+//stała z nazwami czcionek do innych skryptów
 export const FONTNAMES = ["tiny5",'jersey'];
 
 
-
+//aktualny poziom
 var currentLevel = 1;
 
 
+//funkcja rozpoczynająca grę
 window.startGame = function(){
-    MAP.loadLevel(currentLevel);
-    MAP.instantiateEntities(ENTITYHANDLER);
+    MAP.loadLevel(currentLevel); //ładuje poziom
+    MAP.instantiateEntities(ENTITYHANDLER); //ładuje byty
     PLAYER.resetPosition(MAP); //Ustawienie pozycji gracza na START na mapie
 
 
@@ -55,7 +64,7 @@ window.startGame = function(){
         const y = event.clientY - rect.top;
 
         checkHudClick(x, y, PLAYER,MAP,ENTITYHANDLER,AUDIOSYSTEM);
-    });
+    });//sprawdzanie kliknięcia na canvasie ekwipunku
 
     statsCanvas.addEventListener('mousemove', (event) => {
         const rect = statsCanvas.getBoundingClientRect();
@@ -64,9 +73,9 @@ window.startGame = function(){
         const y = event.clientY - rect.top;
 
         checkHudHover(x,y,statsCtx,PLAYER,AUDIOSYSTEM);
-    });
+    });//sprawdzanie czy mysz znajduje się na canvasie ekwipunku
 
-    const GAME = createGame(ctx, statsCtx, gameCanvas, statsCanvas, MAP, PLAYER, CAMERA, KEYS, ENTITYHANDLER, AUDIOSYSTEM);
-    GAME.start();
+    const GAME = createGame(ctx, statsCtx, gameCanvas, statsCanvas, MAP, PLAYER, CAMERA, KEYS, ENTITYHANDLER, AUDIOSYSTEM); 
+    GAME.start(); //inicjalizacja gry
 
 }

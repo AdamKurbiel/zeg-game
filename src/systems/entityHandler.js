340ecf26a91e3de4bd848a2681ba783cd32dcb80
Adam Kurbiel <65848860+AdamKurbiel@users.noreply.github.com>
2026-05-15 14:52:52 +0200

    Added comments to entityHandler.js

--- src/systems/entityHandler.js
+++ src/systems/entityHandler.js
@@ -1,10 +1,14 @@
+//ten skrypt odpowiada za koordynowanie wszystkimi bytami w grze.
 import { Bat } from "../entities/bat.js";
 
+//indeks bytów wraz z informacjami o nich
 const ENTITY_INDEX = {
     "<" : {
         Name : "Bat",
-        Frames : ["BAT0","BAT1"],
-        WidthScale : 1,
+        Frames : ["BAT0","BAT1"], //klatki animacji
+        
+        //możliwość zmiany szerokości/wysokości i koordynatów renderowanej tekstury bytu
+        WidthScale : 1, 
         HeightScale : 0.9,
         YOffset : 0,
         XOffset : 0,
@@ -12,7 +16,7 @@ const ENTITY_INDEX = {
     },
 }
 
-
+//Sprawdzenie, czy podany znak z mapy to jakiś byt.
 export function isEntity(type){
     if (ENTITY_INDEX[type] == undefined){
         return false;
@@ -21,12 +25,14 @@ export function isEntity(type){
     }
 }
 
+
 export class EntityHandler{
     constructor(){
-        this.entities = [],
-        this.move_delay = 100
+        this.entities = [], //lista z wszystkimi aktualnymi bytami
+        this.move_delay = 100 //opóźnienie między aktualizacjami bytó
     }
 
+    //usuwanie bytów z mapy w przypadku czyszczenia
     clear(map){
         for (let entity of this.entities){
             entity.panic(map)
@@ -34,19 +40,22 @@ export class EntityHandler{
         this.entities = [];
     }
 
+    //aktualizacja wszystkich bytów
     update(map, now, move_delay){
         for (let entity of this.entities){
             entity.update(map,now,this.move_delay);
         }
     }
 
+    //dodanie nowego bytu
     addEntity(type,y,x){
         if (type == "<"){
             var bat = new Bat(x,y);
             this.entities.push(bat);
         }        
     }
-    
+
+    //zdobycie informacji z ENTITY_INDEX na podstawie konkretnego typu
     getEntityInfo(type){
         return ENTITY_INDEX[type];
     }
